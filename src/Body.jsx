5904ede952e5acdcc19b7d5b83afd4a7c84bb7f3
Saddam Hussein <work.saddam786@gmail.com>
2025-08-02 11:03:41 +0530

    add footer component

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Body = () => {
   return (
     <div>
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
