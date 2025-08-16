461fb4483ffb0d67b43b4e9fc8f422a67db5d714
Saddam Hussein <work.saddam786@gmail.com>
2025-08-16 08:26:29 +0530

    feat: sending & recieving message

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -5,37 +5,58 @@ import { useSelector } from "react-redux";
 
 const Chat = () => {
   const { targetUserId } = useParams();
-  const [message, setMessage] = useState([{ text: "Hello World" }]);
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
   const user = useSelector((store) => store.user);
   const userId = user?._id;
 
   useEffect(() => {
+    if (!user) return;
+
     const socket = createSocketConnection();
     // As soon as the page load, socket connection is made & joinChat event is emmited.
-    socket.emit("joinChat", { userId, targetUserId });
+    socket.emit("joinChat", {
+      firstName: user.firstName,
+      userId,
+      targetUserId,
+    });
+
+    socket.on("messageRecieved", ({ firstName, text }) => {
+      // console.log(firstName + ": " + text);
+      setMessages((messages) => [...messages, { firstName, text }]);
+    });
 
     // when the component unmount, we must have to close the socket!!
     return () => {
       socket.disconnect();
     };
-  }, []);
+  }, [userId, targetUserId]);
+
+  const sendMessage = () => {
+    const socket = createSocketConnection();
+    socket.emit("sendMessage", {
+      firstName: user.firstName,
+      userId,
+      targetUserId,
+      text: newMessage,
+    });
+    setNewMessage("");
+  };
 
   return (
-    <div className="w-full md:w-2/3 lg:w-1/2 mx-auto m-6 flex flex-col rounded-lg shadow-lg border border-gray-700 bg-base-200 h-[75vh]">
+    <div className="w-full md:w-3/4 mx-auto m-6 flex flex-col rounded-lg shadow-lg border border-gray-700 bg-base-200 h-[75vh]">
       <h1 className="border-b border-gray-600 p-5 ">Chat</h1>
 
       {/* Chat Messages */}
       <div className="flex-1 overflow-y-auto p-5 space-y-4">
-        {message.map((msg, index) => {
+        {messages.map((msg, index) => {
           return (
             <div key={index} className="chat chat-start">
               <div className="chat-header text-sm mb-1">
-                Obi-Wan Kenobi
+                {msg.firstName}
                 <time className="text-xs opacity-50 ml-2">2 hours ago</time>
               </div>
-              <div className="chat-bubble max-w-xs break-words">
-                You were the Chosen!
-              </div>
+              <div className="chat-bubble max-w-xs break-words">{msg.text}</div>
               <div className="chat-footer opacity-50 text-xs">Seen</div>
             </div>
           );
@@ -47,9 +68,13 @@ const Chat = () => {
         <input
           type="text"
           placeholder="Write a message..."
-          className="input rounded-md border border-gray-300 focus:outline-none focus:border-info w-full "
+          className="input rounded-md border border-gray-300 focus:outline-none focus:border-info w-full"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
         />
-        <button className="btn btn-soft btn-info">Send</button>
+        <button onClick={sendMessage} className="btn btn-soft btn-info">
+          Send
+        </button>
       </div>
     </div>
   );
