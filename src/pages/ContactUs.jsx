9615ba14b4bb9cbdc18ff24dc09f93d5f772ca08
Saddam Hussein <work.saddam786@gmail.com>
2025-10-10 10:56:40 +0530

    add contact-us page

---
+++ src/pages/ContactUs.jsx
@@ -0,0 +1,25 @@
+const ContactUs = () => {
+  return (
+    <div className="min-h-screen bg-base-200 flex justify-center p-6">
+      <div className="card w-full max-w-4xl bg-base-100 shadow-xl p-6 prose">
+        <h1 className="font-semibold text-2xl mb-4">Contact Us</h1>
+
+        <p className="font-semibold text mt-4">
+          If any query, feel free to contact us!
+        </p>
+
+        <p className="font-semibold text mt-4">
+          For any assistance and queries, mail us at
+          <strong> work.saddam786@gmail.com</strong>.
+        </p>
+
+        <p className="font-semibold text mt-4">
+          Our Operational Address at: DevTinder, Chennai, Tamil Nadu, India –
+          600003
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ContactUs;
